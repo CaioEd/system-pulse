9c7e999fbc94ba0656476e01bfc8a1c622c7a946
CaioEd <caioersb@gmail.com>
2026-01-15 14:46:14 -0300

    server models/enum

---
+++ src/main/java/com/remote/system_pulse/model/Server.java
@@ -0,0 +1,58 @@
+package com.remote.system_pulse.model;
+import com.remote.system_pulse.model.enums.ServerStatus;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "servers")
+public class Server {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @NotBlank
+    @Pattern(regexp = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$",
+             message = "IP inválido (IPv4 ou IPv6)")
+    @Column(nullable = false)
+    private String ip;
+
+    @NotNull(message = "Port is required")
+    @Min(value = 1, message = "Port must be greater than 0")
+    @Max(value = 65535, message = "Port must be less than 65535")
+    private Integer port;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ServerStatus status = ServerStatus.UNKNOWN;
+
+    @Column(name = "last_checked")
+    private LocalDateTime lastChecked;
+
+}
---
+++ src/main/java/com/remote/system_pulse/model/enums/ServerStatus.java
@@ -0,0 +1,9 @@
+package com.remote.system_pulse.model.enums;
+
+// enums are used to define a fixed set of constants
+public enum ServerStatus {
+    ONLINE,
+    OFFLINE,
+    MAINTENANCE,
+    UNKNOWN
+}
