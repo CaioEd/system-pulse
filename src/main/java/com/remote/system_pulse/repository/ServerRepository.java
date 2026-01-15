74fea274b6ed0e1314a10381ad171e38ba967202
CaioEd <caioersb@gmail.com>
2026-01-15 16:27:20 -0300

    servers repository

---
+++ src/main/java/com/remote/system_pulse/repository/ServerRepository.java
@@ -0,0 +1,10 @@
+package com.remote.system_pulse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.remote.system_pulse.model.Server;
+
+@Repository
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    // Standard CRUD operations are already implemented
+}
