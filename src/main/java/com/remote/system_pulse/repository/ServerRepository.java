65762ccb51d70b2b6122c4f537813bd0f75d707b
CaioEd <caioersb@gmail.com>
2026-01-16 14:33:33 -0300

    server service operations

--- src/main/java/com/remote/system_pulse/repository/ServerRepository.java
+++ src/main/java/com/remote/system_pulse/repository/ServerRepository.java
@@ -1,10 +1,14 @@
 package com.remote.system_pulse.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.remote.system_pulse.model.Server;
 
 @Repository
 public interface ServerRepository extends JpaRepository<Server, Long> {
-    // Standard CRUD operations are already implemented
+    // Standard CRUD operations are already implemented save(), findById(), findAll(), deleteById()
+
+    Optional<Server> findByIpAddress(String ip);
 }
--- src/main/java/com/remote/system_pulse/service/ServerService.java
+++ src/main/java/com/remote/system_pulse/service/ServerService.java
@@ -1,24 +1,42 @@
 package com.remote.system_pulse.service;
 
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 import com.remote.system_pulse.model.Server;
 import com.remote.system_pulse.repository.ServerRepository;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ServerService {
 
     private final ServerRepository serverRepository;
 
+    // Constructor Injection - best practice
     public ServerService(ServerRepository serverRepository) {
         this.serverRepository = serverRepository;
     }
 
-    @Transactional
+    @Transactional  // Transactional is used in case of failure, the transaction will be rolled back
     public Server createServer(Server server) {
         return serverRepository.save(server);
+    }
+
+    @Transactional(readOnly = true)     // readOnly can avoid overhead in read operations
+    public List<Server> getAllServers() {
+        return serverRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Server getServerById(Long id) {
+        return serverRepository.findById(id).
+            orElseThrow(() -> new RuntimeException("Server not found with id: " + id));
+    }
+
+    @Transactional(readOnly = true)
+    public Server getServerByIpAddress(String ip) {
+        return serverRepository.findByIpAddress(ip).orElseThrow(() -> new RuntimeException("Server not found with ip: " + ip));
     }
     
 }
